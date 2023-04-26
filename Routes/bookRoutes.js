2483083723e665726567b384cc96584981c2785c
Diyararashid123 <diyararashid@gmail.com>
2023-04-27 00:26:12 +0200

    first commit

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,21 @@
+// bookRoutes.js
+const express = require('express');
+
+const {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook
+} = require('../controllers/bookController'); // Adjust the path based on your project structure
+
+const router = express.Router();
+
+// Book-related routes
+router.get('/books', getAllBooks);
+router.get('/book/:id', getBookById);
+router.post('/book/create', createBook);
+router.put('/book/update/:id', updateBook);
+router.delete('/book/delete/:id', deleteBook);
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const cors = require('cors');
+const bookRoutes = require('./Routes/bookRoutes'); // Import the bookRoutes
+
+const port = 3000;
+const app = express();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
+app.use(cors());
+
+app.use(bookRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
