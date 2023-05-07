9bfc447c7d2409d2d34f8fce3bba0432ad528464
Diyararashid123 <diyararashid@gmail.com>
2023-05-08 00:03:29 +0200

    thirteen

--- Controllers/bookcontroller.js
+++ Controllers/bookcontroller.js
@@ -52,7 +52,27 @@
     }
   };
   
+  async function create() {
+    const book = {
+      title: "Asdwff Lover",
+      price: 1299.99,
+      description: "Hello test test",
+      categories: [1], // Replace with an array of actual category IDs
+    };
   
+    const response = await fetch("https://bookstore-git-main-diyararashid123.vercel.app/book/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
+    });
+  
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  create();
   
 
   const updateBook = async (req, res) => {
