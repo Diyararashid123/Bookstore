7c720f1e76f4246ff561a6664f979b12a403ba21
Diyararashid123 <diyararashid@gmail.com>
2023-06-19 02:16:04 +0200

    authtest

--- Routes/reviewRoutes.js
+++ Routes/reviewRoutes.js
@@ -1,6 +1,7 @@
     // viewRouter.js
     const express = require('express');
     const { ClerkExpressWithAuth } = require ("@clerk/clerk-sdk-node");
+    const cors = require('cors');
     const {
       createReview, 
       deleteReview, 
@@ -8,8 +9,7 @@
     } = require('../Controllers/reviewcontroller.js'); // Adjust the path 
 
     const router = express.Router();
-   
-
+    router.use(cors());
     // Review-related routes
     router.post('/review/create', ClerkExpressWithAuth(), createReview);
     router.delete('/review/delete/:id', deleteReview);
